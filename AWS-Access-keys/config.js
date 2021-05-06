995aabc47b7ec5833c93453c4f2edb23082bcfb8
sv09 <shreyavinod09@gmail.com>
2021-05-06 20:20:08 +0000

    adding AWS config file to gitignore

--- AWS-Access-keys/config.js
+++
@@ -1,7 +0,0 @@
-const awsConfig = {
-    "accessKeyId" : "AKIAVENC66SR2WEBRCVW",
-    "secretAccessKey" : "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", 
-    "region" : "us-east-2"
-}
-
-module.exports = awsConfig;
